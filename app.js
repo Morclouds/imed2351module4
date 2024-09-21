49124c08cd73285e2eef26b4640615c459b6a37a
Morgan <143568735+Morclouds@users.noreply.github.com>
2024-09-20 19:48:08 -0500

    everything

---
+++ app.js
@@ -0,0 +1,40 @@
+ let text = document.getElementById("title").innerHTML;
+  document.getElementById("title").innerHTML =
+  text.toUpperCase();
+
+const minNum = 1;
+const maxNum = 30;
+const answer = 22;
+
+let attempts = 0;
+let guess;
+let running = true;
+
+
+guess = window.prompt (` Guess a number between ${minNum} - ${maxNum}`);
+guess = Number(guess);
+console.log(typeof guess, guess);
+
+if(isNaN(guess)){
+window.alert("Please enter a valid number"); 
+}
+
+else if(guess < minNum || guess > maxNum){
+  window.alert("Please enter a valid number");
+
+}
+
+else{
+  attempts++;
+  if(guess < answer){
+    window.alert( "TOO LOW! TRY AGAIN!");
+  }
+  else if(guess > answer){
+    window.alert("TOO HIGH! TRY AGAIN!");
+  }
+  else{
+    window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts.`);
+  }
+}
+
+
