24f0a7b416dd9e19bde634b001d743cedba8c80e
Morgan <143568735+Morclouds@users.noreply.github.com>
2024-09-20 20:01:23 -0500

    both

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
  let text = document.getElementById("title").innerHTML;
   document.getElementById("title").innerHTML =
   text.toUpperCase();
+  
+  const welcome = "Hi enjoy this"
+  const item = "guessing number game "
+  console.log(`${welcome} ${item}`)
 
 const minNum = 1;
 const maxNum = 30;
@@ -10,7 +14,7 @@ let attempts = 0;
 let guess;
 let running = true;
 
-
+while(running){
 guess = window.prompt (` Guess a number between ${minNum} - ${maxNum}`);
 guess = Number(guess);
 console.log(typeof guess, guess);
@@ -34,7 +38,11 @@ else{
   }
   else{
     window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts.`);
+    running = false;
   }
+
+  
+}
 }
 
 
